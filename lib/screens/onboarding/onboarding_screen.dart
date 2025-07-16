0dcdc73d2d2203702af73398eab24f79e24129e0
24shrouk <shroukhegab3@gmail.com>
2025-07-16 12:50:06 +0300

    feat(splach screen): create splach screen create colors file create fonts file create asset constant file create screens folder create onboarding folder

---
+++ lib/screens/onboarding/onboarding_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnBoardingScreen extends StatelessWidget {
+  const OnBoardingScreen({super.key});
+  static const onBoardingScreenName = 'OnBoardingScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
